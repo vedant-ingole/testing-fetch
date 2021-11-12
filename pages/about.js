49521cb9ee828b77d2240baacd9aee303b468479
Vedant Ingole <ivedant.11@gmail.com>
2021-11-12 19:34:06 +0530

    fetching data

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const about = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <Link href="/">
+                Home 
+            </Link>
+        </div>
+    )
+}
+
+export default about
